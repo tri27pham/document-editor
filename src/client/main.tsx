27b1550aa038ac3df733673c9bea776bfc473950
tri27pham <tri.pham2002@outlook.com>
2026-02-28 13:54:04 +0000

    refactor: use size constants in css

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { initCssVars } from "./initCssVars";
 import "./styles.css";
+
+initCssVars();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
